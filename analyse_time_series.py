ba44f5c56bd5bbe972d6a0c6a3b24e9717bbdd58
jdvorak <jakub.dvorak@natur.cuni.cz>
2023-07-10 18:07:10 +0200

    Added S1 detection to plots

--- analyse_time_series.py
+++ analyse_time_series.py
@@ -1,6 +1,6 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 import rasterio
 import rasterio.plot
 
@@ -21,8 +21,8 @@ def compute_cfar_treshold(x, y):
     # RMSE of the linear fit
     fit_rmse = mean_squared_error(y, model.predict(x), squared=False)
     # Probability of false alarm
-    k = 1
-    return coh_fit + k * fit_rmse
+    k = 3e-7
+    return coh_fit, k * fit_rmse
 
 def potential_mown_dates(x, y, year=2021):
     df_mown = pd.DataFrame(columns=['date', 'coh', 'confidence'])
@@ -33,10 +33,10 @@ def potential_mown_dates(x, y, year=2021):
     for idx in range(r, len(x)):
         # create doy converison here
         x_doy = [[int(i.strftime('%j'))] for i in x[idx-r:idx]]
-        treshold = compute_cfar_treshold(x_doy, y[idx-r:idx])
-        if y[idx] > treshold:
-            confidence = 1 - 1 * np.exp(-y[idx])
-            df_row = pd.DataFrame([[x[idx],y[idx],confidence]], columns=df_mown.columns)
+        fit_prev_idx, false_alarm = compute_cfar_treshold(x_doy, y[idx-r:idx])
+        if y[idx] > (fit_prev_idx + false_alarm):
+            confidence = 1 - 1 * np.exp(-(y[idx]-fit_prev_idx))
+            df_row = pd.DataFrame([[x[idx],y[idx],confidence[0]]], columns=df_mown.columns)
             df_mown = pd.concat([df_mown, df_row])
 
     df_mown = df_mown.reset_index(drop=True)
@@ -71,6 +71,7 @@ class Time_series:
         self.year = year
         self.relative_orbits = relative_orbits
         self.sar_gdf = gpd.read_file(join(ROOT_DIR, f'reference_data/{str(year)}/vyjezdy_{year}_zonal_stats.gpkg'))
+        self.orig_gdf = gpd.read_file(join(ROOT_DIR, f'reference_data/{str(year)}/vyjezdy_{year}_4326_singlepart.gpkg'))
 
         self.sar_keys = dict.fromkeys(relative_orbits)
         self.sar_dates = dict.fromkeys(relative_orbits)
@@ -79,6 +80,7 @@ class Time_series:
             self.sar_dates[ro] = list(set([datetime.strptime(key[7:16], '%Y_%m%d') for key in self.sar_keys[ro]]))
             self.sar_dates[ro].sort()
         self.metrics = self._extract_metrics()
+        self.detections = {}
 
         self.obs_gdf = self.sar_gdf.filter(regex='\A\d.*\d\Z', axis='columns')
         self.obs_dates = [datetime.strptime(f'{year}_{date}', '%Y_%d_%m') for date in self.obs_gdf.keys()]
@@ -107,21 +109,29 @@ class Time_series:
         ax2 = ax1.twinx()
         ax2.set_ylabel('In situ grass height [cm]', color = 'green')
         ax2.set_ylim(0,120)
+        # Adding potential mowing date ranges
+        plot_reference_mowdates(ax2, plot_id)
+        # Adding bars for in-situ measurements
         plot_2 = ax2.bar(self.obs_dates, self.obs_gdf.iloc[plot_id], width=2, color='green')
         ax2.tick_params(axis ='y', labelcolor = 'green')
 
+        # Adding detected ranges
+        plot_sentinel_mowdates(ax2, self.detections[plot_id])
+
     def _add_map_axis(self, ax_map, plot_idx):
 
-        self.sar_gdf.iloc[[plot_idx]].boundary.plot(ax=ax_map)
+        self.orig_gdf.iloc[[plot_idx]].boundary.plot(ax=ax_map)
         xlim, ylim = ax_map.get_xlim(), ax_map.get_ylim()
 
         rasterio.plot.show(self.dmr, contour=True, ax=ax_map, levels=60, alpha = 0.7,
                            vmin=550, vmax=850, transform=self.dmr_transform, cmap='Greys_r')
-        self.sar_gdf.boundary.plot(ax=ax_map, color='black')
-        self.sar_gdf.iloc[[plot_idx]].boundary.plot(ax=ax_map, color='green')
+        self.orig_gdf.boundary.plot(ax=ax_map, color='#00008b')
+        self.orig_gdf.iloc[[plot_idx]].boundary.plot(ax=ax_map, color='#4cbb17')
         map_buffer = 0.005
         ax_map.set_xlim(xlim[0] - map_buffer, xlim[1] + map_buffer)
         ax_map.set_ylim(ylim[0] - map_buffer, ylim[1] + map_buffer)
+        ax_map.tick_params(axis='x', labelrotation=45)
+        ax_map.ticklabel_format(style='plain')
 
 
     def plot_single_ro(self, plot_idx=0, pol='VH', ro=146, stats=('mean','median','std')):
@@ -148,13 +158,13 @@ class Time_series:
         plt.show()
 
     def plot_single_roi(self, plot_idx=0, pol='VH', relative_orbits=(22,73,95,146), stat='mean'):
-        fig, (ax1, ax_map) = plt.subplots(1, 2, figsize=(20,5))
-        fig.suptitle(f'Coherence {stat} for plot #{plot_idx}')
+        fig, (ax1, ax_map) = plt.subplots(1, 2, figsize=(14,5.5), layout='tight')
+        fig.suptitle(f'{pol} coherence {stat} for plot #{plot_idx}')
 
         ax1.set_xlabel('Time')
         ax1.set_ylabel('S1 Coherence', color = 'black')
-        #ax1.set_ylim(0.25,0.5)
         ax1.tick_params(axis ='y', labelcolor = 'black')
+        self.detections[plot_idx] = {}
 
         for ro in relative_orbits:
             metric = f'{stat}_{pol}_RO-{ro:03}'
@@ -164,7 +174,9 @@ class Time_series:
             # temp for extraction
             mown = identify_mown_dates(self.sar_dates[ro], y_coh, year=self.year)
             ax1.plot(mown['date'], mown['coh'], marker='o', fillstyle='none', color='black', linestyle='')
+            self.detections[plot_idx][ro] = mown
         ax1.legend(loc='upper left')
+        ax1.set_ylim([0,.6])
 
         # Add twin axis for in-situ measurements
         self._add_twinx_axis(ax1, plot_idx)
@@ -172,7 +184,7 @@ class Time_series:
         self._add_map_axis(ax_map, plot_idx)
 
         # Show plot
-        plt.show()
+        plt.show(block=False)
 
     def plot_series_all(self, stat='mean', pol='VH', ro=146):
         fig, (axs) = plt.subplots(ncols=6, nrows=10, squeeze=False, sharex='all', sharey='all')
@@ -185,6 +197,126 @@ class Time_series:
             axs_flat[idx].plot(x, y)
 
 
+def read_heights(in_path):
+    def _str_to_date(in_str):
+        day, month = in_str.split('_')
+        return date(int(year), int(month), int(day))
+
+    year = in_path.split('.')[0][-16:-12]
+    in_gdf = gpd.read_file(in_path)
+
+    gdf_in_situ = in_gdf.filter(regex='\A\d.*\d\Z', axis='columns')
+    gdf_as_dates = gdf_in_situ.rename(columns=_str_to_date)
+    gdf_sorted = gdf_as_dates[sorted(gdf_as_dates)]
+
+    return gdf_sorted
+
+def overall_mown_date(gdf, threshold=20):
+    cols = gdf.columns
+
+    # first date
+    col_0 = gdf[cols[0]]
+    conditions = [
+        (col_0 <= 10),
+        (col_0 <= 20) & (col_0 > 10)
+        ]
+    values = [
+        cols[0] - timedelta(days=14),
+        cols[0] - timedelta(days=21)
+        ]
+    gdf[f'possible_mow_date_before_{cols[0]}'] = np.select(conditions, values)
+
+    # all other dates
+    for idx in range(len(cols) - 1):
+        condition_mown = (gdf[cols[idx]] >= gdf[cols[idx+1]] + threshold)
+        col_1 = gdf[cols[idx+1]]
+        days_since_last = (cols[idx+1]-cols[idx]).days
+        conditions = [
+            condition_mown & (col_1 <= 20),
+            condition_mown & (col_1 <= 40) & (col_1 > 20)
+            ]
+        values = [
+            cols[idx+1] - timedelta(days=min(128, days_since_last)),
+            cols[idx+1] - timedelta(days=min(128, days_since_last))
+            ]
+        gdf[f'possible_mow_date_before_{cols[idx+1]}'] = np.select(conditions, values)
+    return gdf
+
+def plot_one(ax, gdf, id=4):
+    gid = gdf.loc[id,:]
+    cols = gdf.columns
+
+    max_val = gid[:int((len(gdf.columns)+1)/2)].max()
+    max_val = 10
+
+    for idx in range(int((len(gdf.columns))/2)):
+        start = gid.loc[cols[idx+int((len(gdf.columns)+1)/2)]]
+        end = cols[idx]
+        #x_vals = [gid[cols[idx]]]
+        #print(x_vals)
+        if start != 0:
+            x_vals = np.array([start, end, end, start], dtype=np.datetime64)
+            y_vals = [0, 0, max_val, max_val]
+            #ax.fill_between()
+            ax.fill(x_vals, y_vals, alpha=0.25, color='#a8e4a0')
+
+def plot_reference_mowdates(ax, id):
+    gdf_heights = read_heights(inpath_vector)
+    gdf_mowdates = overall_mown_date(gdf_heights)
+    plot_one(ax, gdf_mowdates, id)
+
+
+def assign_confidences(df):
+    for idx_0 in range(len(df)):
+        df.loc[idx_0, 'detection_count'] = 0
+        for idx_1 in range(len(df)):
+            if df.loc[idx_0, 'date'] <= df.loc[idx_1, 'date'] < (df.loc[idx_0, 'date'] + timedelta(days=12)):
+                df.loc[idx_0, 'detection_count'] += 1
+    df = df.sort_values(by='detection_count', ascending=False)
+
+
+    out_dates = pd.DataFrame(columns=['date', 'detection_count'])
+    while len(df) > 0:
+        df = df.reset_index(drop=True)
+        out_dates = pd.concat([out_dates, df.loc[0, ['date', 'detection_count']].to_frame().T], ignore_index=True)
+
+        df = df.drop(
+            df[
+                (df.loc[:, 'date'] >= df.loc[0, 'date']) &
+                (df.loc[:, 'date'] < (df.loc[0, 'date'] + timedelta(days=12)))
+            ].index)
+
+    return out_dates
+
+
+def plot_mowdates(ax, gdf):
+    for _, row in gdf.iterrows():
+        start = row['date'] - timedelta(days=12)
+        end = row['date']
+        x_vals = np.array([start, end, end, start], dtype=np.datetime64)
+        y_vals = [0, 0, 5, 5]
+        if row['detection_count'] == 4:
+            color = 'green'
+        if row['detection_count'] == 3:
+            color = 'yellow'
+        if row['detection_count'] == 2:
+            color = 'orange'
+        if row['detection_count'] == 1:
+            color = 'red'
+        ax.fill(x_vals, y_vals, alpha=0.5, color=color)
+
+
+def plot_sentinel_mowdates(ax, df_dict):
+    dates_combined = pd.DataFrame(columns=['date', 'coh', 'confidence', 'detection_count'])
+    for value in df_dict.values():
+        dates_combined = pd.concat([dates_combined, value])
+    dates_combined = dates_combined.sort_values(by='date')
+    dates_combined = dates_combined.reset_index(drop=True)
+
+    confidence_detections = assign_confidences(dates_combined)
+    plot_mowdates(ax, confidence_detections)
+
+
 if __name__ == '__main__':
     ROOT_DIR = '/media/sf_JD/DP'
     ROOT_DIR = r'C:\Users\dd\Documents\NATUR_CUNI\_dp'
@@ -192,5 +324,8 @@ if __name__ == '__main__':
     inpath_vector = join(ROOT_DIR, f'reference_data/{year}/vyjezdy_{year}_zonal_stats.gpkg')
 
     hello_world = Time_series(year)
-    for i in range(60):
-        hello_world.plot_single_roi(plot_idx=i, relative_orbits=(22,73,95,146), stat='median')
+    #hello_world.plot_single_roi(plot_idx=12, relative_orbits=(22,73,95,146), stat='median', pol='VH')
+    #hello_world.plot_single_roi(plot_idx=12, relative_orbits=(22,73,95,146), stat='median', pol='VV')
+    for i in range(61):
+        hello_world.plot_single_roi(plot_idx=i, relative_orbits=(22,73,95,146), stat='median', pol='VH')
+        hello_world.plot_single_roi(plot_idx=i, relative_orbits=(22,73,95,146), stat='median', pol='VV')
